8da0aa244c8c02c3c67bfb5df5be2fcab962f3c9
daylapin <117829093+anhnhat110@users.noreply.github.com>
2025-05-16 20:45:51 +0700

    Update userService.js

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = `${import.meta.env.REACT_APP_API_URL}/users`;
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/users`;
 
 const userService = {
   createUser: async (userData) => {
